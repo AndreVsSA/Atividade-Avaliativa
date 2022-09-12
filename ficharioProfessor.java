e4796bfd30fb810c4520ade3120cd89cd7d421f8
André Vasconcelos <111195307+Lexnorth@users.noreply.github.com>
2022-09-12 14:43:50 -0300

    Delete ficharioProfessor.java

--- ficharioProfessor.java
+++
@@ -1,8 +0,0 @@
-package br.com.appdahora.poo.ficharios;
-
-public class ficharioProfessor {
-    // para manutencao nos professores
-    // deve herdar de pessoas , acrescentar String registro
-    // deve ter os metodos cadastrar, alterar, excluir, consultar e relatorio
-    // não permitir exclusão de professores vinculados a turmas
-}
