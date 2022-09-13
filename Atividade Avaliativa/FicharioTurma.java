7c1aae667554ce22eca47a200334e2db0671ed15
André Vasconcelos <111195307+Lexnorth@users.noreply.github.com>
2022-09-12 22:42:34 -0300

    Delete Atividade Avaliativa directory

--- Atividade Avaliativa/FicharioTurma.java
+++
@@ -1,6 +0,0 @@
-package br.com.appdahora.poo.ficharios;
-
-public class FicharioTurma {
-    // para manutencao nas turmas
-    // deve ter os metodos cadastrar, alterar, excluir, consultar e relatorio
-}
